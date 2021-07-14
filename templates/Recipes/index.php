a4374c100fe336e0bab8cf995d8a47961d82be80
Marcelo Rocha <contato@omarcelo.com.br>
2021-07-14 17:41:30 -0300

    Added recipes index page and retrieving data from couch db

---
+++ templates/Recipes/index.php
@@ -0,0 +1,22 @@
+<div class="row">
+    <?php foreach ($recipes as $recipe):?>
+    <div class="col-sm-12 col-md-6 col-lg-4 mb-4">
+        <div class="card">
+            <?= $this->Html->image($recipe->photo_url, [
+                'class' => "card-img-top"
+            ])?>
+            <div class="card-body">
+                <h5 class="card-title"><?= h($recipe->name)?></h5>
+                <p class="card-text"><?= h($recipe->description ?? __('A good recipe'))?></p>
+                <?= $this->Html->link(
+                    __('Tell me how'),
+                    [
+                        'action' => 'view',
+                    ],
+                    ['class' => 'btn btn-primary']
+                )?>
+            </div>
+        </div>
+    </div>
+    <?php endforeach;?>
+</div>
